8d6d8a617d372f77d4199bbb5c0fa2580d313ae4
Tzu-Tang Hsu <vichsu416@gmail.com>
2017-07-07 11:37:32 -0400

    Final

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var methodOverride = require('method-override');
 
-var port = process.env.PORT || 3000;
+var PORT = process.env.PORT || 3000;
 
 var app = express();
 
@@ -25,4 +25,6 @@ var routes = require('./controllers/burgers_controller.js');
 
 app.use('/', routes);
 
-app.listen(port);
+app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+  });
